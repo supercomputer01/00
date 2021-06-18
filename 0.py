ba260cd31133522418b32b713229bc6625d1f3a0
supercomputer01 <86040972+supercomputer01@users.noreply.github.com>
2021-06-18 19:33:31 +0800

    Create 0.py

---
+++ 0.py
@@ -0,0 +1,14 @@
+input('system alert level 0')=alert level
+
+if alert level = ('no alert'):
+       print ('no alert anounced')
+
+elif alert level = ('yellow alert'):
+       print ('yellow alert)'
+
+elif alert level = ('red alert'):
+       print ('red alert')
+
+else :
+       print ('black alerts')
+              
